3164957b3b95181d5c35984a9885b0d1e6cc3e35
unknown <se.mailforwork@gmail.com>
2018-08-10 19:25:54 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Import required modules
+# Import dataset
+# Import inception model
+# Create and compile the model by joining the decoder(RNN model) to the encoder (CNN model)
+# Train the model on flikr dataset
+# Test the model on images

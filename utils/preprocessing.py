74d6dbf0df70b51ca6538ff4f19088d1dee63f0d
unknown <se.mailforwork@gmail.com>
2018-08-11 21:46:35 +0530

    For preprocessing the data

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,45 +1,38 @@
 import numpy as np
 import string
 from os import listdir
-from keras.applications.vgg16 import VGG16, preprocess_input
+from pickle import dump
+from keras.applications.inception_v3 import preprocess_input
 from keras.preprocessing.image import load_img, img_to_array
-from keras.models import Model
 
 #The function returns a dictionary of image identifier to image features.
 def extract_features(path):
-	model = VGG16()
-	model.layers.pop()
-	model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
-	print(model.summary())
-	# extract features from each photo
-	features = dict()
-	for name in listdir(path):
-		# load an image from file
-		filename = path + '/' + name
-		image = load_img(filename, target_size=(224, 224))
-		# convert the image pixels to a numpy array
-		image = img_to_array(image)
-		# reshape data for the model
-		image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-		# prepare the image for the VGG model
-		image = preprocess_input(image)
-		# get features
-		feature = model.predict(image, verbose=0)
-		# get image id
-		image_id = name.split('.')[0]
-		# store feature
-		features[image_id] = feature
+ 	model = defineCNNmodel()
+ 	# extract features from each photo
+ 	features = dict()
+ 	for name in listdir(path):
+ 		# load an image from file
+ 		filename = path + '/' + name
+ 		image = load_img(filename, target_size=(299, 299))
+ 		# convert the image pixels to a numpy array
+ 		image = img_to_array(image)
+ 		# reshape data for the model
+ 		image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+ 		# prepare the image for the VGG model
+ 		image = preprocess_input(image)
+ 		# get features
+ 		feature = model.predict(image, verbose=0)
+ 		# get image id
+ 		image_id = name.split('.')[0]
+ 		# store feature
+ 		features[image_id] = feature
 	return features
 
-# load doc into memory
-def load_doc(filename):
-	file = open(filename, 'r')
-	text = file.read()
-	file.close()
-	return text
-
 # extract descriptions for images
-def load_descriptions(doc):
+def load_descriptions(filename):
+	file = open(filename, 'r')
+	doc = file.read()
+	file.close()
 	mapping = dict()
 	# process lines by line
 	for line in doc.split('\n'):
@@ -79,14 +72,6 @@ def clean_descriptions(descriptions):
 			# store as string
 			desc_list[i] =  ' '.join(desc)
 
-# convert the loaded descriptions into a vocabulary of words
-def to_vocabulary(descriptions):
-	# build a list of all description strings
-	all_desc = set()
-	for key in descriptions.keys():
-		[all_desc.update(d.split()) for d in descriptions[key]]
-	return all_desc
-
 # save descriptions to file, one per line
 def save_descriptions(descriptions, filename):
 	lines = list()
@@ -98,28 +83,28 @@ def save_descriptions(descriptions, filename):
 	file.write(data)
 	file.close()
 
-# extract features from all images
-path = 'Flicker8k_Dataset'
-features = extract_features(path)
-print('Extracted Features: %d' % len(features))
+def preprocessData():
+	# extract features from all images
+	path = 'Flicker8k_Dataset'
+	print('Generating image features...')
+	features = extract_features(path)
+	print('Completed. Saving now...')
+	# save to file
+	dump(features, open('features.pkl', 'wb'))
+	print("Save Complete.")
 
-# load descriptions containing file
-filename = 'Flickr8k_text/Flickr8k.token.txt'
-doc = load_doc(filename)
+	# load descriptions containing file and parse descriptions
+	descriptions_path = 'Flickr8k_text/Flickr8k.token.txt'
 
-# parse descriptions
-descriptions = load_descriptions(doc)
-print('Loaded: %d ' % len(descriptions))
+	descriptions = load_descriptions(descriptions_path)
+	print('Loaded Descriptions: %d ' % len(descriptions))
 
-# clean descriptions
-clean_descriptions(descriptions)
+	# clean descriptions
+	clean_descriptions(descriptions)
 
-# summarize vocabulary to get idea of size of our vocab
-vocabulary = to_vocabulary(descriptions)
-print('Vocabulary Size: %d' % len(vocabulary))
+	# save descriptions
+	save_descriptions(descriptions, 'descriptions.txt')
 
-# save descriptions
-save_descriptions(descriptions, 'descriptions.txt')
 
 # Now descriptions.txt is of form :
 # Example : 2252123185_487f21e336 stadium full of people watch game

670874b6ee0173c7b25f147a8fee47bb663dac28
unknown <se.mailforwork@gmail.com>
2018-08-11 21:47:09 +0530

    Module for defining,compiling and loading model

--- utils/model.py
+++ utils/model.py
@@ -1,18 +1,36 @@
+from numpy import argmax
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.image import load_img, img_to_array
+from nltk.translate.bleu_score import corpus_bleu
 
 
-# define the captioning model
-def define_model(vocab_size, max_length):
+# define the CNN model
+def defineCNNmodel():
+	model = InceptionV3()
+ 	model.layers.pop()
+ 	model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+ 	#print(model.summary())
+ 	return model
+
+# define the RNN model
+def defineRNNmodel(vocab_size, max_length):
 	# feature extractor model
-	inputs1 = Input(shape=(4096,))
-	fe1 = Dropout(0.5)(inputs1)
-	fe2 = Dense(256, activation='relu')(fe1)
+	inputs1 = Input(shape=(2048,))
+	fe1 = Dense(256, activation='relu')(inputs1)
 	# sequence model
 	inputs2 = Input(shape=(max_length,))
 	se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
-	se2 = Dropout(0.5)(se1)
-	se3 = LSTM(256)(se2)
+	se2 = LSTM(256)(se1)
 	# decoder model
-	decoder1 = add([fe2, se3])
+	decoder1 = add([fe1, se2])
 	decoder2 = Dense(256, activation='relu')(decoder1)
 	outputs = Dense(vocab_size, activation='softmax')(decoder2)
 	# tie it together [image, seq] [word]
@@ -20,5 +38,53 @@ def define_model(vocab_size, max_length):
 	model.compile(loss='categorical_crossentropy', optimizer='adam')
 	# summarize model
 	print(model.summary())
-	plot_model(model, to_file='model.png', show_shapes=True)
 	return model
+
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+	return None
+
+# generate a description for an image, given a pre-trained model and a tokenizer to map integer back to word
+def generate_desc(model, tokenizer, photo, max_length):
+	# seed the generation process
+	in_text = 'startseq'
+	# iterate over the whole length of the sequence
+	for i in range(max_length):
+		# integer encode input sequence
+		sequence = tokenizer.texts_to_sequences([in_text])[0]
+		# pad input
+		sequence = pad_sequences([sequence], maxlen=max_length)
+		# predict next word
+		yhat = model.predict([photo,sequence], verbose=0)
+		# convert probability to integer
+		yhat = argmax(yhat)
+		# map integer to word
+		word = word_for_id(yhat, tokenizer)
+		# stop if we cannot map the word
+		if word is None:
+			break
+		# append as input for generating the next word
+		in_text += ' ' + word
+		# stop if we predict the end of the sequence
+		if word == 'endseq':
+			break
+	return in_text
+
+
+def evaluate_model(model, photos, descriptions, tokenizer, max_length):
+	actual, predicted = list(), list()
+	
+	for key, desc_list in descriptions.items():
+		yhat = generate_desc(model, tokenizer, photos[key], max_length)
+		references = [d.split() for d in desc_list]
+		actual.append(references)
+		predicted.append(yhat.split())
+
+	print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+	print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+	print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+	print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))

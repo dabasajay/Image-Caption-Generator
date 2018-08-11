32847cc94a2acb82a723b27405bba91c9e08bd2e
unknown <se.mailforwork@gmail.com>
2018-08-11 21:44:59 +0530

    For training model from scratch and validating it

--- train_val.py
+++ train_val.py
@@ -1,17 +1,40 @@
+from pickle import load
 from utils.model import *
+from utils.load_data import *
 
+# Load Data
+# X1 : image features
+# X2 : text features
+X1train, X2train, max_length 	=	loadTrainData(path = 'Flickr8k_text/Flickr_8k.trainImages.txt',preprocessDataReady=False)
 
+X1val, X2val = loadValData(path = 'Flickr_8k.devImages.txt')
 
-# define the model
-model = define_model(vocab_size, max_length)
+# load the tokenizer
+tokenizer_path = 'tokenizer.pkl'
+tokenizer = load(open(tokenizer_path, 'rb'))
+vocab_size = len(tokenizer.word_index) + 1
 
-# define checkpoint callback
-filepath = 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', 
-								verbose=1, save_best_only=True, 
-								mode='min')
+# prints 34
+print('Max Length : ',max_length) 
 
-# fit model
-model.fit([X1train, X2train], ytrain, 
-			epochs=20, verbose=2, callbacks=[checkpoint], 
-			validation_data=([X1val, X2val], yval))
+# We already have the image features from CNN model so we only need to define the RNN model now.
+# define the RNN model
+model = defineRNNmodel(vocab_size, max_length)
+
+# train the model, run epochs manually and save after each epoch
+epochs = 10
+steps_train = len(X2train)
+steps_val = len(X2val)
+for i in range(epochs):
+    # create the train data generator
+    generator_train = data_generator(X1train, X2train, tokenizer, max_length)
+    # create the val data generator
+    generator_val = data_generator(X1val, X2val, tokenizer, max_length)
+    # fit for one epoch
+    model.fit_generator(generator_train, epochs=1, steps_per_epoch=steps_train, 
+                        verbose=1, validation_data=generator_val, validation_steps=steps_val)
+    # save model
+    model.save('model_' + str(i) + '.h5')
+
+# Evaluate the model on validation data and ouput BLEU score
+# evaluate_model(model, X1val, X2val, tokenizer, max_length)

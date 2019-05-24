9e4144c50cd3371cc25281715ec35b0056748769
Ajay Dabas <se.dabasajay@gmail.com>
2019-05-24 12:34:57 +0530

    more comments for better readability and understanding

--- utils/load_data.py
+++ utils/load_data.py
@@ -9,12 +9,18 @@ from keras.utils import to_categorical
 	*We have Flickr_8k.trainImages.txt and Flickr_8k.devImages.txt files which consist of unique identifiers(id) 
 		which can be used to filter the images and their descriptions
 	*Load a pre-defined list of photo identifiers(id)
+	*Glimpse of file:
+		2513260012_03d33305cf.jpg
+		2903617548_d3e38d7f88.jpg
+		3338291921_fe7ae0c8f8.jpg
+		488416045_1c6d903fe0.jpg
+		2644326817_8f45080b87.jpg
 '''
 def load_set(filename):
 	file = open(filename, 'r')
 	doc = file.read()
 	file.close()
-	dataset = list()
+	ids = list()
 	# Process line by line
 	for line in doc.split('\n'):
 		# Skip empty lines
@@ -22,87 +28,107 @@ def load_set(filename):
 			continue
 		# Get the image identifier(id)
 		_id = line.split('.')[0]
-		dataset.append(_id)
-	return set(dataset)
+		ids.append(_id)
+	return set(ids)
 
 '''
 	*The model we'll develop will generate a caption for a given photo and the caption will be generated one word at a time. 
 	*The sequence of previously generated words will be provided as input. Therefore, we will need a ‘first word’ to 
 		kick-off the generation process and a ‘last word‘ to signal the end of the caption.
-	*We'll use the strings ‘startseq‘ and ‘endseq‘ for this purpose. These tokens are added to the loaded descriptions
+	*We'll use the strings ‘startseq‘ and ‘endseq‘ for this purpose. These tokens are added to the captions
 		as they are loaded. 
 	*It is important to do this now before we encode the text so that the tokens are also encoded correctly.
-	*Load clean descriptions into memory
+	*Load captions into memory
+	*Glimpse of file:
+		1000268201_693b08cb0e child in pink dress is climbing up set of stairs in an entry way
+		1000268201_693b08cb0e girl going into wooden building
+		1000268201_693b08cb0e little girl climbing into wooden playhouse
+		1000268201_693b08cb0e little girl climbing the stairs to her playhouse
+		1000268201_693b08cb0e little girl in pink dress going into wooden cabin
 '''
-def load_clean_descriptions(filename, dataset):
+def load_cleaned_captions(filename, ids):
 	file = open(filename, 'r')
 	doc = file.read()
 	file.close()
-	descriptions = dict()
+	captions = dict()
+	_count = 0
 	# Process line by line
 	for line in doc.split('\n'):
 		# Split line on white space
 		tokens = line.split()
-		# Split id from description
-		image_id, image_desc = tokens[0], tokens[1:]
-		# Skip images not in the set
-		if image_id in dataset:
+		# Split id from caption
+		image_id, image_caption = tokens[0], tokens[1:]
+		# Skip images not in the ids set
+		if image_id in ids:
 			# Create list
-			if image_id not in descriptions:
-				descriptions[image_id] = list()
-			# Wrap description in tokens
-			desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
-			# store
-			descriptions[image_id].append(desc)
-	return descriptions
+			if image_id not in captions:
+				captions[image_id] = list()
+			# Wrap caption in start & end tokens
+			caption = 'startseq ' + ' '.join(image_caption) + ' endseq'
+			# Store
+			captions[image_id].append(caption)
+			_count = _count+1
+	return captions, _count
 
+# Load image features
+def load_image_features(filename, ids):
+	# load all features
+	all_features = load(open(filename, 'rb'))
+	# filter features
+	features = {_id: all_features[_id] for _id in ids}
+	return features
+
+# Convert a dictionary to a list
+def to_lines(captions):
+	all_captions = list()
+	for image_id in captions.keys():
+		[all_captions.append(caption) for caption in captions[image_id]]
+	return all_captions
 
 '''
-	*The description text will need to be encoded to numbers before it can be presented to the model.
-	*The first step in encoding the data is to create a consistent mapping from words to unique integer values.
-		Keras provides the Tokenizer class that can learn this mapping from the loaded description data.
+	*The captions will need to be encoded to numbers before it can be presented to the model.
+	*The first step in encoding the captions is to create a consistent mapping from words to unique integer values.
+		Keras provides the Tokenizer class that can learn this mapping from the loaded captions.
+	*Fit a tokenizer on given captions
 '''
-# Convert a dictionary of clean descriptions to a list of descriptions
-def to_lines(descriptions):
-	all_desc = list()
-	for key in descriptions.keys():
-		[all_desc.append(d) for d in descriptions[key]]
-	return all_desc
-
-# Fit a tokenizer on given caption descriptions
-def create_tokenizer(descriptions):
-	lines = to_lines(descriptions)
+def create_tokenizer(captions):
+	lines = to_lines(captions)
 	tokenizer = Tokenizer()
 	tokenizer.fit_on_texts(lines)
 	return tokenizer
 
+# Calculate the length of the captions with the most words
+def calc_max_length(captions):
+	lines = to_lines(captions)
+	return max(len(line.split()) for line in lines)
+
 '''
-	*Each description will be split into words. The model will be provided one word & the photo and it generates the next word. 
-	*Then the first two words of the description will be provided to the model as input with the image to generate the next word. 
+	*Each caption will be split into words. The model will be provided one word & the photo and it generates the next word. 
+	*Then the first two words of the caption will be provided to the model as input with the image to generate the next word. 
 	*This is how the model will be trained.
 	*For example, the input sequence “little girl running in field” would be 
 		split into 6 input-output pairs to train the model:
 
 		X1		X2(text sequence) 								y(word)
 		-----------------------------------------------------------------
-		photo	startseq, 									little
+		photo	startseq,										little
 		photo	startseq, little,								girl
-		photo	startseq, little, girl, 						running
-		photo	startseq, little, girl, running, 				in
-		photo	startseq, little, girl, running, in, 			field
-		photo	startseq, little, girl, running, in, field, 	endseq
+		photo	startseq, little, girl,							running
+		photo	startseq, little, girl, running,				in
+		photo	startseq, little, girl, running, in,			field
+		photo	startseq, little, girl, running, in, field,		endseq
 '''
-
 # Create sequences of images, input sequences and output words for an image
-def create_sequences(tokenizer, max_length, desc_list, photo):
+def create_sequences(tokenizer, max_length, captions_list, photo):
 	# X1 : input for photo features
 	# X2 : input for text features
+	# y  : output word
 	X1, X2, y = list(), list(), list()
 	vocab_size = len(tokenizer.word_index) + 1
-	# Walk through each description for the image
-	for desc in desc_list:
+	# Walk through each caption for the image
+	for caption in captions_list:
 		# Encode the sequence
-		seq = tokenizer.texts_to_sequences([desc])[0]
+		seq = tokenizer.texts_to_sequences([caption])[0]
 		# Split one sequence into multiple X,y pairs
 		for i in range(1, len(seq)):
 			# Split into input and output pair
@@ -117,56 +143,42 @@ def create_sequences(tokenizer, max_length, desc_list, photo):
 			y.append(out_seq)
 	return np.array(X1), np.array(X2), np.array(y)
 
-# Calculate the length of the description with the most words
-def calc_max_length(descriptions):
-	lines = to_lines(descriptions)
-	return max(len(d.split()) for d in lines)
-
-# Load photo features
-def load_photo_features(filename, dataset):
-	# load all features
-	all_features = load(open(filename, 'rb'))
-	# filter features
-	features = {k: all_features[k] for k in dataset}
-	return features
-
 # Data generator, intended to be used in a call to model.fit_generator()
-def data_generator(photos, descriptions, tokenizer, max_length):
-	# loop for ever over images
-	while 1:
-		for key, desc_list in descriptions.items():
-			# retrieve the photo feature
-			photo = photos[key][0]
-			in_img, in_seq, out_word = create_sequences(tokenizer, max_length, desc_list, photo)
-			yield [[in_img, in_seq], out_word]
+def data_generator(images, captions, tokenizer, max_length):
+	# Loop infinitely over images
+	while True:
+		for image_id, captions_list in captions.items():
+			# Retrieve the image features
+			image = images[image_id][0]
+			input_img, input_sequence, output_word = create_sequences(tokenizer, max_length, captions_list, image)
+			yield [[input_img, input_sequence], output_word]
 
-def loadTrainData(config,preprocessDataReady=True):
-	train = load_set(config['train_data_path'])
-	print('Training Dataset Length: %d' % len(train))
+def loadTrainData(config):
+	train_image_ids = load_set(config['train_data_path'])
 	# Check if we already have preprocessed data saved and if not, preprocess the data.
-	if preprocessDataReady is False:
-		preprocessData(config)
-	# Descriptions
-	train_descriptions = load_clean_descriptions(config['model_save_path']+'descriptions.txt', train)
-	print('Descriptions for Training = %d' % len(train_descriptions))
-	# Photo features
-	train_features = load_photo_features(config['model_save_path']+'features.pkl', train)
-	print('Photos for Training = %d' % len(train_features))
-	# Prepare tokenizer
-	tokenizer = create_tokenizer(train_descriptions)
-	# Save the tokenizer
-	dump(tokenizer, open(config['model_save_path']+'tokenizer.pkl', 'wb'))
+	# Create and save 'captions.txt' & features.pkl
+	preprocessData(config)
+	# Load captions
+	train_captions, _count = load_cleaned_captions(config['model_data_path']+'captions.txt', train_image_ids)
+	# Load image features
+	train_image_features = load_image_features(config['model_data_path']+'features.pkl', train_image_ids)
+	print('{}: Available images for training: {}'.format(mytime(),len(train_image_features)))
+	print('{}: Available captions for training: {}'.format(mytime(),_count))
+	if not os.path.exists(config['model_data_path']+'tokenizer.pkl'):
+		# Prepare tokenizer
+		tokenizer = create_tokenizer(train_captions)
+		# Save the tokenizer
+		dump(tokenizer, open(config['model_data_path']+'tokenizer.pkl', 'wb'))
 	# Determine the maximum sequence length
-	max_length = calc_max_length(train_descriptions)
-	return train_features, train_descriptions, max_length
+	max_length = calc_max_length(train_captions)
+	return train_image_features, train_captions, max_length
 
 def loadValData(config):
-	val = load_set(config['val_data_path'])
-	print('Validation Dataset Length: %d' % len(val))
-	# Descriptions
-	val_descriptions = load_clean_descriptions(config['model_save_path']+'descriptions.txt', val)
-	print('Descriptions for Validation = %d' % len(val_descriptions))
-	# Photo features
-	val_features = load_photo_features(config['model_save_path']+'features.pkl', val)
-	print('Photos for Validation = %d' % len(val_features))
-	return val_features, val_descriptions
+	val_image_ids = load_set(config['val_data_path'])
+	# Load captions
+	val_captions, _count = load_cleaned_captions(config['model_data_path']+'captions.txt', val_image_ids)
+	# Load image features
+	val_features = load_image_features(config['model_data_path']+'features.pkl', val_image_ids)
+	print('{}: Available images for validation: {}'.format(mytime(),len(val_features)))
+	print('{}: Available captions for validation: {}'.format(mytime(),_count))
+	return val_features, val_captions

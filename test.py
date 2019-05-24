d54c37f492400ee1cd413a0f12f2817f9ea2f44c
Ajay Dabas <se.dabasajay@gmail.com>
2019-05-24 12:38:14 +0530

    minor changes

--- test.py
+++ test.py
@@ -5,49 +5,49 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.inception_v3 import preprocess_input
-from utils.model import CNNModel, generate_desc
+from utils.model import CNNModel, generate_caption
+import os
 
 from config import config
 
-# extract features from each photo in the directory
+# Extract features from each photo in the directory
 def extract_features(filename,model):
-	# load the photo
+	# Loading and resizing image because input size for CNN model used here (InceptionV3) is 299x299
 	image = load_img(filename, target_size=(299, 299))
-	# convert the image pixels to a numpy array
+	# Convert the image pixels to a numpy array
 	image = img_to_array(image)
-	# reshape data for the model
+	# Reshape data for the model
 	image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-	# prepare the image for the Inceptionv3 model
+	# Prepare the image for the CNN Model (Inceptionv3) model
 	image = preprocess_input(image)
-	# get features
+	# Pass image into model to get encoded features
 	features = model.predict(image, verbose=0)
 	return features
 
-# load the tokenizer
+# Load the tokenizer
 tokenizer_path = config['tokenizer_path']
 tokenizer = load(open(tokenizer_path, 'rb'))
 
-# max sequence length (from training)
+# Max sequence length (from training)
 max_length = config['max_length']
 
-# load the model from last epoch
-model_path = config['model_save_path']+'model_epoch_'+str(config['num_of_epochs']-1)+'.h5'
-caption_model = load_model(model_path)
+# Load the model
+caption_model = load_model(config['model_load_path'])
 
 image_model = CNNModel()
 
-# load and prepare the photograph
+# Load and prepare the image
 for image_file in os.listdir(config['test_data_path']):
 	if(image_file.split('.')[1]=='jpg' or image_file.split('.')[1]=='jpeg'):
 		image = extract_features(config['test_data_path']+image_file,image_model)
-		# generate description
-		description = generate_desc(caption_model, tokenizer, image, max_length)
-		# remove startseq and endseq
-		caption = 'Caption: ' + description.split()[1].capitalize()
-		for x in description.split()[2:len(description.split())-1]:
+		# Generate caption
+		caption = generate_caption(caption_model, tokenizer, image, max_length)
+		# Remove startseq and endseq
+		caption = 'Caption: ' + caption.split()[1].capitalize()
+		for x in caption.split()[2:len(caption.split())-1]:
 		    caption = caption + ' ' + x
 		caption += '.'
-		# Show image and it's caption
+		# Show image and its caption
 		pil_im = Image.open(config['test_data_path']+image_file, 'r')
 		fig, ax = plt.subplots(figsize=(8, 8))
 		ax.get_xaxis().set_visible(False)

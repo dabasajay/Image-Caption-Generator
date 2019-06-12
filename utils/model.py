7c39297b6d5216094fd08d8f4ca31b9e5bd19eed
Ajay Dabas <se.dabasajay@gmail.com>
2019-06-12 07:55:34 +0530

    added beam search caption generation and evaluation

--- utils/model.py
+++ utils/model.py
@@ -97,6 +97,7 @@ def int_to_word(integer, tokenizer):
 
 """
 	*Generate a caption for an image, given a pre-trained model and a tokenizer to map integer back to word
+	*Uses simple argmax
 """
 def generate_caption(model, tokenizer, image, max_length):
 	# Seed the generation process
@@ -126,12 +127,60 @@ def generate_caption(model, tokenizer, image, max_length):
 	return in_text
 
 """
-	*Evaluate the model on BLEU Score
+	*Generate a caption for an image, given a pre-trained model and a tokenizer to map integer back to word
+	*Uses BEAM Search algorithm
+"""
+def generate_caption_beam_search(model, tokenizer, image, max_length, beam_index=3):
+	# in_text --> [[idx,prob]] ;prob=0 initially
+	in_text = [[tokenizer.texts_to_sequences(['startseq'])[0], 0.0]]
+	while len(in_text[0][0]) < max_length:
+		tempList = []
+		for seq in in_text:
+			padded_seq = pad_sequences([seq[0]], maxlen=max_length)
+			preds = model.predict([image,padded_seq], verbose=0)
+			# Take top (i.e. which have highest probailities) `beam_index` predictions
+			top_preds = np.argsort(preds[0])[-beam_index:]
+			# Getting the top `beam_index` predictions and 
+			for word in top_preds:
+				next_seq, prob = seq[0][:], seq[1]
+				next_seq.append(word)
+				# Update probability
+				prob += preds[0][word]
+				# Append as input for generating the next word
+				tempList.append([next_seq, prob])
+		in_text = tempList
+		# Sorting according to the probabilities
+		in_text = sorted(in_text, reverse=False, key=lambda l: l[1])
+		# Take the top words
+		in_text = in_text[-beam_index:]
+	in_text = in_text[-1][0]
+	final_caption = [int_to_word(i,tokenizer) for i in in_text]
+	return ' '.join(final_caption)
+
+"""
+	*Evaluate the model on BLEU Score using argmax predictions
 """
 def evaluate_model(model, images, captions, tokenizer, max_length):
 	actual, predicted = list(), list()
 	for image_id, caption_list in tqdm(captions.items()):
 		yhat = generate_caption(model, tokenizer, images[image_id], max_length)
+		ground_truth = [caption.split() for caption in caption_list]
+		actual.append(ground_truth)
+		predicted.append(yhat.split())
+	print('BLEU Scores :')
+	print('A perfect match results in a score of 1.0, whereas a perfect mismatch results in a score of 0.0.')
+	print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+	print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+	print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+	print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
+
+"""
+	*Evaluate the model on BLEU Score using BEAM search predictions
+"""
+def evaluate_model_beam_search(model, images, captions, tokenizer, max_length, beam_index=3):
+	actual, predicted = list(), list()
+	for image_id, caption_list in tqdm(captions.items()):
+		yhat = generate_caption_beam_search(model, tokenizer, images[image_id], max_length, beam_index=beam_index)
 		ground_truth = [caption.split() for caption in caption_list]
 		actual.append(ground_truth)
 		predicted.append(yhat.split())
